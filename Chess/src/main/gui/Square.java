739549b65f31f3f4a90ea76c09d005d582d943ea
Jeffrey <jr926@drexel.edu>
2015-02-17 15:48:30 -0500

    Starting user interface for Chess

--- Chess/src/main/gui/Square.java
+++ Chess/src/main/gui/Square.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 
 public class Square extends JButton implements ActionListener {
 	/**
