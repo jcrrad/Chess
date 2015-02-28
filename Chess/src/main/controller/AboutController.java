ee490dcfdb4173d0237367f71ff793146f555914
Stephen Cantwell <scantwell4@gmail.com>
2015-02-28 15:26:04 -0500

    Added View interface and skeleton for about controller

---
+++ Chess/src/main/controller/AboutController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import gui.View;
+import core.client.Model;
+
+public class AboutController extends Controller {
+
+	public AboutController(Model model, View view) {
+		super(model, view);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
