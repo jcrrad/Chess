5337de54ad50e010c3eec83b1e795c10d5e625c9
Stephen Cantwell <scantwell4@gmail.com>
2015-03-03 15:51:42 -0500

    Added Message Object

--- Chess/src/main/core/client/Message.java
+++ Chess/src/main/core/client/Message.java
@@ -1,10 +1,61 @@
 package core.client;
 
 public class Message {
+	
+	private boolean boardUpdate = false;
+	private boolean hasChat = false;
+	private String chatText;
+	private boolean clientsTurn = false;
+	private boolean disconnected = false;
 
-	public boolean isServerMessage() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isClientsTurn() 
+	{
+		return this.clientsTurn;
 	}
+	
+	public void setClientsTurn(boolean isClientsTurn) 
+	{
+		this.clientsTurn = isClientsTurn;
+	}
+	
+	public boolean hasChat() 
+	{
+		return hasChat;
+	}
+
+	public boolean hasBoardUpdate() 
+	{
+		return boardUpdate;
+	}
+	
+	public void setChatText(String msg)
+	{
+		this.hasChat = true;
+		this.chatText = msg;
+	}
+	
+	public String getChatText()
+	{
+		return this.chatText;
+	}
+
+	public Object getBoard() 
+	{
+		return null;
+	}
+
+	public boolean hasReconnected() {
+		return !disconnected;
+	}
+
+	public boolean isDisconnected() {
+		return disconnected;
+	}
+
+	public void setDisconnected(boolean disconnected) {
+		this.disconnected = disconnected;
+	}
+
+
 
 }
