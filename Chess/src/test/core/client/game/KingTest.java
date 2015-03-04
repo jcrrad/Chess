a7f90b6dd87dfbff83d024da3fefcaacdd1b24da
Michael Brisbin <mb3354@drexel.edu>
2015-03-03 22:07:56 -0500

    King tests completed

--- Chess/src/test/core/client/game/KingTest.java
+++ Chess/src/test/core/client/game/KingTest.java
@@ -1,6 +1,12 @@
 package core.client.game;
 
-import core.client.game.King;
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+
+import core.client.Coordinate;
 
 /**
  * Base test class for {@link King}
@@ -9,5 +15,103 @@ import core.client.game.King;
  */
 public class KingTest 
 {
+	
+	@Test
+	public void testGetSymbol_black()
+	{
+		Coordinate coord = new Coordinate();
+		coord.setX(3);
+		coord.setY(3);
+		King king = new King(null, Color.BLACK, coord);
+		assertEquals("K", king.getSymbol());
+	}
+	
+	@Test
+	public void testGetSymbol_white()
+	{
+		Coordinate coord = new Coordinate();
+		coord.setX(3);
+		coord.setY(3);
+		King king = new King(null, Color.WHITE, coord);;
+		assertEquals("k", king.getSymbol());
+	}
+	
+	@Test
+	public void testCanMove_oneSquareVert()
+	{
+		Coordinate kingLoc = new Coordinate();
+		kingLoc.setX(3);
+		kingLoc.setY(3);
+		King king = new King(null, Color.BLACK, kingLoc);
+		Coordinate moveTo = new Coordinate();
+		moveTo.setX(4);
+		moveTo.setY(3);
+		assertTrue(king.canMove(moveTo));
+	}
+	
+	@Test
+	public void testCanMove_oneSquareHoriz()
+	{
+		Coordinate kingLoc = new Coordinate();
+		kingLoc.setX(3);
+		kingLoc.setY(3);
+		King king = new King(null, Color.BLACK, kingLoc);
+		Coordinate moveTo = new Coordinate();
+		moveTo.setX(3);
+		moveTo.setY(4);
+		assertTrue(king.canMove(moveTo));
+	}
+	
+	@Test
+	public void testCanMove_oneSquareDiag()
+	{
+		Coordinate kingLoc = new Coordinate();
+		kingLoc.setX(3);
+		kingLoc.setY(3);
+		King king = new King(null, Color.BLACK, kingLoc);
+		Coordinate moveTo = new Coordinate();
+		moveTo.setX(4);
+		moveTo.setY(4);
+		assertTrue(king.canMove(moveTo));
+	}
+	
+	@Test
+	public void testCanMove_twoSquaresVert()
+	{
+		Coordinate kingLoc = new Coordinate();
+		kingLoc.setX(3);
+		kingLoc.setY(3);
+		King king = new King(null, Color.BLACK, kingLoc);
+		Coordinate moveTo = new Coordinate();
+		moveTo.setX(5);
+		moveTo.setY(3);
+		assertFalse(king.canMove(moveTo));
+	}
+	
+	@Test
+	public void testCanMove_twoSquaresHoriz()
+	{
+		Coordinate kingLoc = new Coordinate();
+		kingLoc.setX(3);
+		kingLoc.setY(3);
+		King king = new King(null, Color.BLACK, kingLoc);
+		Coordinate moveTo = new Coordinate();
+		moveTo.setX(3);
+		moveTo.setY(5);
+		assertFalse(king.canMove(moveTo));
+	}
+	
+	@Test
+	public void testCanMove_twoSquaresDiag()
+	{
+		Coordinate kingLoc = new Coordinate();
+		kingLoc.setX(3);
+		kingLoc.setY(3);
+		King king = new King(null, Color.BLACK, kingLoc);
+		Coordinate moveTo = new Coordinate();
+		moveTo.setX(5);
+		moveTo.setY(5);
+		assertFalse(king.canMove(moveTo));
+	}
 
 }
