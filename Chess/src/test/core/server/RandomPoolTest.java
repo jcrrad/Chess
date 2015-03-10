f4a7a7be6fe5831de705e085e32a5a1842d34bd4
Michael Brisbin <mb3354@drexel.edu>
2015-03-09 22:37:17 -0400

    ServerTests

--- Chess/src/test/core/server/RandomPoolTest.java
+++ Chess/src/test/core/server/RandomPoolTest.java
@@ -1,5 +1,34 @@
 package core.server;
 
-public class RandomPoolTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RandomPoolTest 
+{
+	@Mock private ServerClient sc1;
+	@Mock private ServerClient sc2;
+
+	@Test
+	public void testAdd_andGetSize()
+	{
+		RandomPool pool = new RandomPool(2);
+		pool.add(sc1);
+		assertEquals(1, pool.getPoolSize());
+	}
+	
+	@Test
+	public void testGetPair()
+	{
+		RandomPool pool = new RandomPool(2);
+		pool.add(sc1);
+		pool.add(sc2);
+		Pair<ServerClient, ServerClient> p = new Pair<ServerClient, ServerClient>(sc1, sc2);
+		assertNotNull(pool.getPair());
+	}
 }
